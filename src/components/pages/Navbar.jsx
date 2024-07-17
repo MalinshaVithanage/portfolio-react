04e16c0455fbf3e368e18be55069d5e637ba0bbd
malinsha.vithanage@inivosglobal.com <kavindigvm.20@uom.lk>
2024-07-17 21:05:03 +0530

    NavBar done

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -2,61 +2,41 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
- function NavBar(){
-  let location = useLocation();
-    return(
-      
-        <Navbar expand="lg" b data-bs-theme="dark" className='nav-bar'>
-        <Container>
 
-          <Navbar.Brand href="/">Portfolio</Navbar.Brand> 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className='nav-list'>
-            <Nav >
-            <CustomNavLink 
-                    href="/"
-                    currentPath={location.pathname}
-                    className='nav-item '
-                  >
-                    Home
-                  </CustomNavLink>
-                  <CustomNavLink
-                    href="/about"
-                    currentPath={location.pathname}
-                    className='nav-item '
-                  >
-                   About
-                  </CustomNavLink>
-                  <CustomNavLink
-                    href="/projects"
-                    currentPath={location.pathname}
-                    className='nav-item '
-                  >
-                   Projects
-                  </CustomNavLink>
-                  <CustomNavLink
-                    href="/contact"
-                    currentPath={location.pathname}
-                    className='nav-item '
-                  >
-                   Contact
-                  </CustomNavLink>
-              {/* <Nav.Link href="/" className='nav-item '>Home</Nav.Link>
-              <Nav.Link href="/about" className='nav-item'>About</Nav.Link>
-              <Nav.Link href="/projects" className='nav-item'>Projects</Nav.Link>
-              <Nav.Link href="/contact" className='nav-item'>Contact</Nav.Link> */}
-             
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+function NavBar() {
+  const location = useLocation();
 
- }
- function CustomNavLink({ href, currentPath, children }) {
-  // Determine if the current link matches the active link
+
+  return (
+    <Navbar expand="sm" bg="dark" variant="dark" className="nav-bar">
+      <Container>
+        <Navbar.Brand href="/">Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav  className="nav-list">
+            <CustomNavLink href="/" currentPath={location.pathname} className="nav-item">
+              Home
+            </CustomNavLink>
+            <CustomNavLink href="/about" currentPath={location.pathname} className="nav-item">
+              About
+            </CustomNavLink>
+            <CustomNavLink href="/projects" currentPath={location.pathname} className="nav-item">
+              Projects
+            </CustomNavLink>
+            <CustomNavLink href="/contact" currentPath={location.pathname} className="nav-item">
+              Contact
+            </CustomNavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+function CustomNavLink({ href, currentPath, children }) {
+
   const isActive = currentPath === href;
 
   return (
@@ -66,4 +46,4 @@ import { useLocation } from "react-router-dom";
   );
 }
 
- export default NavBar;
+export default NavBar;
--- src/components/pages/homepage/HomePage.jsx
+++ src/components/pages/homepage/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import NavBar from '../Navbar';
+import Image from 'react-bootstrap/Image';
 function HomePage() {
     return (
         <div>
           <NavBar/>
+          <section className='hero-section'>
+          <Image src="holder.js/171x180" roundedCircle />
+          </section>
         </div>
     )
 }
